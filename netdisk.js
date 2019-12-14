1ce9cd9ccf1cbd91f9b4958ae7ec2792a4cf4a67
onewayticket255 <37437616+onewayticket255@users.noreply.github.com>
2019-12-14 21:59:58 +0800

    Create netdisk.js

---
+++ netdisk.js
@@ -0,0 +1,40 @@
+let ariaRPC = 'http://localhost:6800/jsonrpc'
+let body = $response.body
+body = JSON.parse(body)
+let downloadurl = body.urls[0].url
+console.log(downloadurl)
+
+let baidu_header = [
+    'User-Agent: netdisk;2.2.3;pc;pc-mac;10.15.2;macbaiduyunguanjia',
+    'X-Download-From: baiduyun'
+]
+
+let options = {
+    url: ariaRPC,
+    header: {
+        'Content-Type': 'application/json'
+    },
+    body: {
+        'jsonrpc': '2.0',
+        'method': 'aria2.addUri',
+        'id': Math.round(new Date() / 1000),
+        'params': [
+            [downloadurl],
+            {
+                'header': baidu_header,
+                'split': 8
+            }
+        ]
+    }
+}
+
+$httpClient.post(options, function (error, response, data) {
+    if (error) {
+        $notification.post('wrong', 'wrong', 'wrong')
+    } else {
+        $notification.post('Success', '', data)
+    }
+
+})
+
+$done({})
